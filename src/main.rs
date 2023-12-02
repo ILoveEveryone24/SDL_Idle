f812dc6197b5f5198ce9e80076b95d30751fe5fd
ILoveEveryone24 <ernestascizas2404@gmail.com>
2023-12-02 08:55:36 +0100

    Idler

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,24 @@ pub fn main() {
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    let button1 = Rect::new(200, 300, 200, 100);
+    let button1 = Rect::new(300, 300, 200, 100);
     let mut leftDown = false;
     let mut button1Down = false;
+
+    let line = Rect::new(150, 100, 500, 50);
+    let mut overline = Rect::new(150, 100, 0, 50);
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
-        canvas.set_draw_color(Color::RGB(2, 64, 255));
+        canvas.set_draw_color(Color::RGB(66, 75, 79));
         canvas.clear();
- 
-        canvas.set_draw_color(Color::RGB(20, 20, 10));
-        canvas.fill_rect(button1).unwrap();
 
+        canvas.set_draw_color(Color::RGB(88, 97,101));
+        canvas.fill_rect(line).unwrap();
+ 
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -56,20 +59,26 @@ pub fn main() {
         }
 
         if leftDown && button1Down {
-            canvas.set_draw_color(Color::RGB(255, 0, 0));
+            canvas.set_draw_color(Color::RGB(215, 120, 192));
             canvas.fill_rect(button1).unwrap();
+           
+            if overline.width() > line.width() {
+                overline.set_width(line.width());
+            } 
+            else{ 
+                overline.set_width(overline.width() + 1);
+            }
+            canvas.set_draw_color(Color::RGB(255, 234, 139));
+            canvas.fill_rect(overline).unwrap();
         } else {
-            canvas.set_draw_color(Color::RGB(20, 20, 10)); 
+            canvas.set_draw_color(Color::RGB(121, 215, 120)); 
             canvas.fill_rect(button1).unwrap();
+            overline.set_width(0);
         }
 
         let state = event_pump.mouse_state();
 
         let buttons:Vec<_> = state.pressed_mouse_buttons().collect();
-
-        if !buttons.is_empty(){
-            println!("{:?}",buttons);
-        }
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
