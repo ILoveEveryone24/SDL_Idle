2c8efe17d9f4548f4b460a7e2934e9bafb56de1b
ILoveEveryone24 <ernestascizas2404@gmail.com>
2023-12-02 14:39:25 +0100

    PNG

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,46 @@ use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 use std::time::Duration;
 use sdl2::rect::Rect;
+use sdl2::image::{InitFlag, LoadTexture};
+
+struct panel {
+    button: Rect,
+    line: Rect,
+    overline: Rect,
+    loot: Rect
+}
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let image_context = sdl2::image::init(InitFlag::PNG).unwrap();
 
-    let window = video_subsystem.window("Melvor Idle Budget", 800, 600)
+    let window = video_subsystem.window("Melvor Idle Budget", 2560, 1600)
         .position_centered()
+        .resizable()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
     let button1 = Rect::new(300, 300, 200, 100);
-    let mut leftDown = false;
     let mut button1Down = false;
 
-    let line = Rect::new(150, 100, 500, 50);
-    let mut overline = Rect::new(150, 100, 0, 50);
+    let loot = Rect::new(300, 10, 200, 200);
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let line = Rect::new(150, 220, 500, 50);
+    let mut overline = Rect::new(150, 220, 0, 50);
+
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("../pictures/bonfire.png").expect("Failed to load PNG");
+
     'running: loop {
         canvas.set_draw_color(Color::RGB(66, 75, 79));
         canvas.clear();
+        
+        canvas.copy(&texture, None, loot);
 
         canvas.set_draw_color(Color::RGB(88, 97,101));
         canvas.fill_rect(line).unwrap();
@@ -43,30 +57,23 @@ pub fn main() {
                     break 'running
                 }
                 Event::MouseButtonDown{x, y, mouse_btn: MouseButton::Left, ..} => {
-                    leftDown = true;
                     if button1.contains_point((x, y)){
-                        button1Down = true;
+                        button1Down = !button1Down;
                     }
-                    else {
-                        button1Down = false;
-                    }
-                }
-                Event::MouseButtonUp {mouse_btn: MouseButton::Left, ..} => {
-                    leftDown = false;
                 }
                 _ => {}
             }
         }
 
-        if leftDown && button1Down {
+        if button1Down {
             canvas.set_draw_color(Color::RGB(215, 120, 192));
             canvas.fill_rect(button1).unwrap();
            
             if overline.width() > line.width() {
-                overline.set_width(line.width());
+                overline.set_width(0);
             } 
             else{ 
-                overline.set_width(overline.width() + 1);
+                overline.set_width(overline.width() + 10);
             }
             canvas.set_draw_color(Color::RGB(255, 234, 139));
             canvas.fill_rect(overline).unwrap();
